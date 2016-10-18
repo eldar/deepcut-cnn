c0af7881832821eb5a026638c4eee39ab0eda522
Stefan Helmert <helme@hrz.tu-chemnitz.de>
2016-10-18 22:24:01 +0200

    added script to install the dependencies and changed CMakeLists.txt for compatibility

---
+++ install_required.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt install -y --allow-unauthenticated libgflags-dev libgoogle-glog-dev protobuf-compiler liblmdb-dev libleveldb-dev libsnappy-dev libatlas-dev libatlas-base-dev cuda-cublas-dev-8-0 nvidia-cuda-toolkit libhdf5-dev libopencv-dev libhdf5-serial-dev libboost-all-dev 
+
+ln -s /usr/include/hdf5/serial/* /usr/include/.
+
